3399c58e455c8bb679549dc668a856bb3be5fa03
Aditi Srinivas <aditi.srinivas11@gmail.com>
2025-04-27 22:44:01 -0700

    Finished Lab

--- Lab12/asrinivas6.cpp
+++ Lab12/asrinivas6.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
-#include <limit>
+#include <climits>
+#include <algorithm> 
 using namespace std;
 
 int main(){
@@ -10,9 +11,12 @@ int main(){
   cin>>V;
   cin>>E;
 
-  int** W = new int[V];
+  int** W = new int*[V];
   for(int i = 0; i < V; i++){
     W[i] = new int[V];
+    for(int j = 0; j < V; j++){
+      W[i][j] = INT_MAX;
+    }
   }
 
   vector<int>* G = new vector<int>[V];
@@ -25,25 +29,42 @@ int main(){
     G[v].push_back(u);
   }
   
-  vector<int> Q;
+  vector<int> Q(V);
+  vector<bool> full(V, false);
+  int start = 0;
   int* parents = new int[V];
   int* keys = new int[V];
   for(int i = 0; i < V; i++){
-    parents[i] = null;
-//    keys[i] = INT_MAX;
-      keys[i] = 1000000;
+    parents[i] = -1;
+    keys[i] = INT_MAX;
+    Q[i] = i;
   }
+  keys[start] = 0;
 
   // set keys[i] to 0
   // put all v into Q
 
   while(!Q.empty()){
     // find vertex which has min in keys: for loop in Q
+    int min = INT_MAX;
+    int u = -1;
+    int position = -1;
+
+    for(int i = 0; i < Q.size(); i++){
+      if(keys[Q[i]] < min){
+        min = keys[Q[i]];
+        u = Q[i];
+        position = i;
+      }
+    }
     // remove element at index 'u' from Q
+    Q.erase(Q.begin() + position);
+    full[u] = true;
+
     for(int v : G[u]){
 //    for(int i = 0; i < G[u].size(); i++){
 //      int v = G[u][i];
-      if(Q.contains(v) && W[u][v] < keys[v]){
+      if(!full[v] && W[u][v] < keys[v]){
         parents[v] = u;
         keys[v] = W[u][v];
       }
@@ -51,6 +72,9 @@ int main(){
   }
   
   // print parents from index 1
+  for(int v = 1; v < V; v++){
+    cout << parents[v] << "\n";
+  }
 
   return 0;
 }
